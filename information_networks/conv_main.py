2e1c8a680e00789528809d2a5495b777528ad1ff
Unknown <vanedamario@gmail.com>
2018-01-10 13:47:35 +0100

    convolutional test

---
+++ information_networks/conv_main.py
@@ -0,0 +1,141 @@
+# TODO: track the values of the loss during traning (early stopping ?)
+#     : the initialization of the weights seems not to affect dramatically the
+#       results, we always check the convergence of the training loss
+#     : is the representation we are getting translational invariant?
+
+import numpy as np
+import tensorflow as tf
+from sklearn.model_selection import StratifiedShuffleSplit
+from tensorflow.contrib.layers import flatten, linear
+from information_networks.signal_generation import signal_gen
+import matplotlib.pyplot as plt
+
+
+def main():
+
+    ############################################################################
+    #                           specifics
+    #   dataset
+    sample_size = int(1e4); features = 1000; n_classes = 3
+    p_pattern = np.array([3, 4, 2]); length_patterns = 50
+    X, y, patterns = signal_gen(sample_size, features, n_classes, p_pattern,
+        length_patterns, noise=0.1)
+
+    X = np.reshape(X, newshape=(X.shape[0], X.shape[1], 1))
+    plt.plot(X[0, :])
+    plt.plot(X[1, :])
+    plt.plot(X[2, :])
+    plt.show()
+    #   training - test sets
+    train_points = 100
+    perc_train = float(train_points)/sample_size
+    sss = StratifiedShuffleSplit(n_splits=2, train_size=perc_train,
+        test_size=1-perc_train)
+    idx_split, _ = sss.split(X, y)
+    idx_train = idx_split[0]; idx_test = idx_split[1]
+    X_train = X[idx_train, :]; y_train = y[idx_train]
+    X_test = X[idx_test, :]; y_test = y[idx_test]
+
+    #   convolutional model characteristics
+    n_filters = 10
+    length_filter = 50
+    n_iters = int(1e3)
+
+    #   build placeholders
+    signals = tf.placeholder(tf.float32)
+    labels = tf.placeholder(tf.int32)
+
+    ############################################################################
+    #                      convolutional algo
+    # this is equivalent to make an average on the length_filter window
+    init_conv_weights = 1./ np.sqrt(length_filter) * np.ones((
+        length_filter, 1, n_filters)).astype("float32")
+    conv_weights = tf.Variable(init_conv_weights)
+
+    convolution = tf.nn.conv1d(signals, filters=conv_weights, stride=1,
+        padding="SAME")
+    flat_convolution = flatten(tf.transpose(convolution, perm=[0, 2, 1]))
+    logits = linear(tf.reshape(flat_convolution, [-1, 1, n_filters * features]),
+        n_classes)
+    # add a linear transformation which takes as input flat_convolution and
+    # with output of dimension (batch, n_classes)
+    loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels,
+        logits=logits)
+    # loss function with softmax computed internally
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-4)
+    # optimize it with gradient descent or other methods (Adam, SGD)
+    train = optimizer.minimize(loss)
+    # train the model
+
+    init_op = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init_op)
+        for n in range(n_iters):
+            sess.run(train, feed_dict={signals: X_train, labels: y_train})
+        filters_values = sess.run(conv_weights)
+        train_loss_value = sess.run(loss, feed_dict={signals: X_train,
+            labels: y_train})
+        test_loss_value = sess.run(loss, feed_dict={signals: X_test,
+            labels: y_test})
+    print("\nCONVOLUTIONAL MODEL")
+    print("loss values on training set: "+str(np.mean(train_loss_value)))
+    print("loss values on test set: "+str(np.mean(test_loss_value)))
+
+    ############################################################################
+    #                       linear algo
+    init_linear_weights = tf.random_normal([features, n_classes])
+    linear_output = linear(tf.reshape(signals, [-1, 1, features]), n_classes)
+    loss_linear_model = tf.nn.sparse_softmax_cross_entropy_with_logits(
+        labels=labels, logits=linear_output)
+    optimizer_linear_model = tf.train.GradientDescentOptimizer(learning_rate=1e-3)
+    train_linear_model = optimizer_linear_model.minimize(loss_linear_model)
+
+    init_op = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init_op)
+        for n in range(n_iters):
+            sess.run(train_linear_model, feed_dict={signals: X_train,
+                labels: y_train})
+        train_loss_value = sess.run(loss_linear_model,
+            feed_dict={signals: X_train, labels: y_train})
+        test_loss_value = sess.run(loss, feed_dict={signals: X_test,
+            labels: y_test})
+    print("\nLINEAR MODEL")
+    print("loss values on training set: "+str(np.mean(train_loss_value)))
+    print("loss values on test set: "+str(np.mean(test_loss_value)))
+
+    ############################################################################
+    #                       plots
+    path_save_fig = "/Users/vanessa/Desktop/"
+    # warning : this is useful if the n_classes == 3
+    f1, (ax1, ax2, ax3) = plt.subplots(1, 3, sharey=True)
+    ax1.plot(X[0, :], label="class 1")
+    ax1.plot(X[1, :])
+    ax2.plot(X[sample_size/2, :], label="class 2")
+    ax2.plot(X[sample_size/2+1, :])
+    ax3.plot(X[-2, :], label="class 3")
+    ax3.plot(X[-1, :])
+    plt.savefig(path_save_fig + "signals.png")
+    plt.close()
+
+    # warning : this works only for the specifics given for the dataset & model
+    f2, axarr = plt.subplots(2, 10)
+    for i in range(n_filters):
+        if i < p_pattern[0]:
+            axarr[0, i].plot(patterns[0][i, :], 'r')
+        elif np.logical_and(i >= 3, i < 7):
+            axarr[0, i].plot(patterns[1][i-3, :], 'b')
+        elif np.logical_and(i >= 7, i < 9):
+            axarr[0, i].plot(patterns[2][i-7, :], 'g')
+
+        axarr[1, i].plot(filters_values[:, 0, i], label="filters "+str(i))
+    plt.legend()
+    plt.savefig(path_save_fig + "truePatterns_convWeights.png")
+    plt.show()
+    plt.close()
+
+    return
+
+
+if __name__ == '__main__':
+    main()
---
+++ information_networks/signal_generation.py
@@ -0,0 +1,103 @@
+# TODO: gaussian amplitude / dilation
+# TODO: remove the sum of patterns
+# TODO: random uniform translation
+import numpy as np
+
+
+def signal_gen(n, p, n_classes, p_pattern, length_patterns, noise):
+    """
+    function for the generation of one dimensional signals for classification
+    tasks. We will generate samples belonging to different classes. each class
+    is characterized by a number of patterns (fixed by the user)
+    These patterns will recur through the signals
+    Parameters:
+        n (int), number of signals
+        p (int), length of the signals
+        n_classes (int), #classes
+        p_pattern (np.array), each element corresponds to the #characteristic
+            for that class
+        length_patterns (int), length for each pattern, equivalent to all
+        noise (float), percentage (with respect to the squared amplitude of the
+            signal), of gaussian additive noise
+    Returns:
+        X (np.array), dim n x p, samples
+        y (np.array), dim n x 1, labels
+        patterns (list of (np.array)),
+            dim (n_classes*[p_pattern, length_patterns]) characteristic patterns
+        w (list of (np.array)),
+            dim (n_classes*[p_pattern, p - length_patterns])
+    """
+
+    if n_classes != p_pattern.size:
+        print("error, #classes and elements specified in the array don't match")
+        exit(-1)
+
+    X = np.zeros((n, p))  # initialized as zero
+    y = np.zeros(n)
+
+    # here we create the patterns. each class has its own frequencies.
+    # each pattern is generated using a sinusoidal function of these f
+    # the pattern is then amplified randomly (multiplied with a constant)
+    patterns = []
+
+
+    lowest_f = np.log10(1. / length_patterns)   # in log scale
+    highest_f = np.log10(1. / 4)
+    # half of the period for the lowest, 1./4 four points for the latter
+    possible_f = np.logspace(lowest_f, highest_f, n_classes*3)
+    possible_f = np.split(possible_f, n_classes)
+    mu = 1.  # mean value of the amplitude for each pattern
+    for k in range(n_classes):
+        f = np.random.choice(possible_f[k], size=p_pattern[k])
+        # generate a sinusoidal signal, different for the different classes
+        sinusoidal = np.zeros((p_pattern[k], length_patterns))
+        for j in range(p_pattern[k]):
+            sinusoidal[j, :] = np.sin(2 * np.pi * f[j] * np.arange(length_patterns))
+        tmp_const =  mu + np.random.randn(p_pattern[k])
+        patterns.append(np.multiply(tmp_const, sinusoidal.T).T)
+
+    # here we create the signal. the patterns will be shifted through the signal
+    # we then need to split p in a number of segment, then we will randomly
+    # select the segment where we want the pattern to appear
+    segments = p / length_patterns
+    samples_per_class = n / n_classes  # sample points for each class
+
+    all_x_in_signal = np.arange(0, segments-1, 2)  # this vector contains all
+    # the segments (with step=2), to avoid overlap
+    all_x_in_segment = np.arange(0, length_patterns)  # all positions in the segment
+    # for each pattern we put a n_patterns_per_signal
+    n_patterns_per_signal = segments/2
+    # we will extract the position using the
+    idx_all_pos = np.arange(n_patterns_per_signal)
+
+    for k in range(n_classes):
+        matrix_x_in_segment = np.random.choice(all_x_in_segment,
+            size=(samples_per_class, n_patterns_per_signal))
+
+        matrix_x_in_signal = np.array([]).reshape(0, n_patterns_per_signal)
+        for j in range(samples_per_class):
+            tmp_x_in_signal = np.random.choice(all_x_in_signal,replace=False,
+                size=n_patterns_per_signal)
+            matrix_x_in_signal = np.vstack((matrix_x_in_signal, tmp_x_in_signal))
+        matrix_x_in_signal = matrix_x_in_signal.astype("int32")
+
+        random_pos = length_patterns * matrix_x_in_signal + matrix_x_in_segment
+        # necessary : we cannot split without control for the #patterns
+        # if array split does not result in an equal division
+        idx_pos_patterns = np.array(np.split(idx_all_pos[:-(idx_all_pos.size%patterns[k].shape[0])],
+            patterns[k].shape[0]))
+
+        for t in range(patterns[k].shape[0]):
+            for i in range(samples_per_class):
+                for s in range(idx_pos_patterns.shape[1]):
+                    start = (random_pos[i, idx_pos_patterns[t, s]])
+                    X[k*samples_per_class+i, start:start+length_patterns] = patterns[k][t, :] * (mu + np.random.randn(1))
+                y[i] = k
+
+    # here we add the gaussian noise
+    if noise>0. :
+        X += noise * np.mean(1./p * np.linalg.norm(X, axis=-1)) * np.random.randn(n, p)
+
+    y = y.astype("int32")
+
+    return X[:samples_per_class * n_classes, :], y[:samples_per_class * n_classes], patterns
